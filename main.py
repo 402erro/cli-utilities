624ace263962e7ca2d088c68b75689ea170f2974
402erro <rowdhafakier@gmail.com>
2025-07-22 22:30:07 +0200

    Created main.py which will will serve as the file which controls the system that the user will be interacting with. Created utils folder which will contain the utility files. Created calculator.py and weather.py which will contain the logic for the respective functionalities.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import os
+class Main:
+    def __init__(self):
+        self.user = os.getlogin()
+        self.UtilityPath = r"C:\Users\rowdh\OneDrive\Documents\Tomfoolery\Personal Project\cli-utilities\utils"
+        self.suffix = ".py"
+    
+    def get_utility_list(self):
+        UtilityList = os.listdir(self.UtilityPath)
+        # print(UtilityList)
+        return UtilityList
 
+    def get_utility_names(self):
+        UtilityList = self.get_utility_list()
+        CleanUtilityList = []
+
+        for Utility in UtilityList:
+            if Utility.endswith(self.suffix):
+                CleanUtilityList.append(Utility[-3:-1])
+
+        # print(CleanUtilityList)
+        return CleanUtilityList
+
+        
+
+    
+    
+if __name__ == "__main__":
+    main = Main()
+    main.get_utils()
